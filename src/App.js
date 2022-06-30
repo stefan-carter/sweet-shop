7805631d2dd0aaf3112c054fb485cb6620efa504
Stefan Carter <stefan.carter@icloud.com>
2022-06-30 15:27:05 +0100

    React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import candy from './media/candy.svg';
+import Header from './components/Header';
+import FooterPage from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <img src={candy} className="App-logo" alt="candy" />
+      <p>Russia already controls a large stretch of Ukraine's Black Sea coast, plus the Crimean Peninsula and the entire Sea of Azov. Holding Snake Island completed an effective blockade of Odesa, and meant exports of the vast majority of Ukrainian grain were impossible.
+
+        It also meant the Black Sea coast became vulnerable to attack too, and military experts in Kyiv voiced fears that Russia could install long-range air defences, such as an S-400 air missile system.
+
+        A look at the map also shows that Russian control of the island represented a threat to Nato member Romania - both its key port of Constanta and traffic in the mouth of the River Danube.
+
+        It is not just strategically significant - this area is also rich in reserves of petroleum and gas.
+        Map of Black Sea</p>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+const Header = () => {
+    return (
+        <h1>Sweet Cheeks</h1>
+    )
+}
+export default Header;
